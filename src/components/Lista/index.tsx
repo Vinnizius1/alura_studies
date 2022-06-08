fdf0b1b94ba583545d7df5ec5772bcbeec6532f5
vinicius <vmm_7@hotmail.com>
2022-06-08 14:00:38 -0300

    término da estilização com SASS

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,3 +1,5 @@
+import "./style.scss";
+
 function Lista() {
   const tarefas = [
     {
@@ -11,12 +13,12 @@ function Lista() {
   ];
 
   return (
-    <aside>
+    <aside className="listaTarefas">
       <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => {
           return (
-            <li key={index}>
+            <li className="item" key={index}>
               <h3>{item.tarefa}</h3>
               <span>{item.tempo}</span>
             </li>
