8dac80c0ec16fae0da22a29142f7b4d9f9d78580
vinicius <vmm_7@hotmail.com>
2022-06-19 10:06:54 -0300

    início do Cronômetro

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Botao from "../Botao";
+import Relogio from "./Relogio";
+import style from "./Cronometro.module.scss";
 
-function Cronometro() {
-  return <div>Cronometro</div>;
+export default function Cronometro() {
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      <div className={style.relogioWrapper}>
+        <Relogio />
+      </div>
+      {/* Exemplo de "props.children", que ao contrário das outras props, é escrita dentro do componente,
+        "abrindo" e "fechando" a tag como em uma tag HTML. */}
+      <Botao>Começar!</Botao>
+    </div>
+  );
 }
-
-export default Cronometro;
