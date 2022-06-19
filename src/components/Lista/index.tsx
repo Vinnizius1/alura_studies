ea5175d5f8b14e7b6e060c056605cfdba5501334
vinicius <vmm_7@hotmail.com>
2022-06-19 10:28:27 -0300

    início do useState no componente Lista

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import Item from "./Item";
-
 import style from "./Lista.module.scss";
 
 function Lista() {
-  const tarefas = [
+
+
+  const [tarefas, setTarefas] = useState([
     {
       tarefa: "React",
       tempo: "02:00:00",
@@ -12,17 +14,24 @@ function Lista() {
       tarefa: "Javascript",
       tempo: "01:00:00",
     },
-  ];
+  ])
 
   return (
     <aside className={style.listaTarefas}>
-      <h2>Estudos do dia</h2>
+      <h2
+        onClick={() => {
+          setTarefas([
+            ...tarefas,
+            { tarefa: "Estudar estado", tempo: "05:00:00" },
+          ]);
+        }}
+      >
+        Estudos do dia
+      </h2>
       <ul>
         {tarefas.map((item, index) => {
-         /*  tempo={item.tempo} tarefa={item.tarefa} */
-          return (
-            <Item {...item} key={index} />
-          );
+          /*  tempo={item.tempo} tarefa={item.tarefa} */
+          return <Item {...item} key={index} />;
         })}
       </ul>
     </aside>
