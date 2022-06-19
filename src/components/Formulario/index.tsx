c017fac988bad045798f984589bd56fc0cf58fd4
vinicius <vmm_7@hotmail.com>
2022-06-19 12:44:12 -0300

    controlando o formulário no Class Component

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,14 +3,29 @@ import Botao from "../Botao";
 import style from "./Formulario.module.scss";
 
 class Formulario extends React.Component {
+  state = {
+    tarefa: "",
+    tempo: "00:00",
+  };
+
+  // MÉTODO
+  adicionarTarefa(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("state: ", this.state);
+  }
+
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
             type="text"
             name="tarefa"
+            value={this.state.tarefa}
+            onChange={(e) =>
+              this.setState({ ...this.state, tarefa: e.target.value })
+            }
             id="tarefa"
             placeholder="O que você quer estudar?"
             required
@@ -22,6 +37,10 @@ class Formulario extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            value={this.state.tempo}
+            onChange={(e) =>
+              this.setState({ ...this.state, tempo: e.target.value })
+            }
             id="tempo"
             min="00:00:00"
             max="01:00:00"
