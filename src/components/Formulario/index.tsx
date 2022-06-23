96481729812bb0ff33c0b1faddd433bcb77cfa51
vinicius <vmm_7@hotmail.com>
2022-06-23 18:21:46 -0300

    Import do 'uuid' + criação das var. 'completado' e 'selecionado'

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import style from "./Formulario.module.scss";
+import { v4 as uuidv4 } from "uuid";
 
-class Formulario extends React.Component<{setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>}> {
+class Formulario extends React.Component<{
+  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>;
+}> {
   state = {
     tarefa: "",
     tempo: "00:00",
@@ -13,7 +16,14 @@ class Formulario extends React.Component<{setTarefas: React.Dispatch<React.SetSt
   adicionarTarefa(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     // console.log("state: ", this.state);
-    this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {...this.state}])
+    this.props.setTarefas((tarefasAntigas) => [
+      ...tarefasAntigas,
+      { ...this.state, selecionado: false, completado: false, id: uuidv4() },
+    ]);
+    this.setState({
+      tarefa: "",
+      tempo: "00:00",
+    });
   }
 
   render() {
